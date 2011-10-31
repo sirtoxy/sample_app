5e575fca9949173a36390df5811eba7d4baf25fd
Anton Khotov <sirtoxy@gmail.com>
2011-10-31 14:38:58 +1100

    Added user factory and tests for show page

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                  "Myname"
+  user.email                 "myname@g.com"
+  user.password              "foobar"
+  user.password_confirmation "foobar"
+end
