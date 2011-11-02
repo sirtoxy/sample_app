85773573b9d10978788afe73bb01b34698093b0d
Anton Khotov <sirtoxy@gmail.com>
2011-11-02 13:07:22 +1100

    Finish sugnup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,16 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @title = @user.name
   end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+     
+      redirect_to @user, :flash => { :success => "Welcome to the sample app"}
+    else
+      @title = "Sign up"
+      render 'new'
+    end
+  end 
+    
 end
