f0a044d1f6b7c661c546e2c72d964c46de9792e1
Anton Khotov <sirtoxy@gmail.com>
2011-11-11 16:47:10 +1100

    Index page with pagination

---
+++ lib/tasks/sample_data.rake
@@ -0,0 +1,19 @@
+require 'faker'
+
+namespace :db do 
+  desc 'Fill database'
+  task :populate => :environment do
+    Rake::Task['db:reset'].invoke
+    User.create!(:name => "Example User", 
+                 :email => "example@railstutorial.org", 
+                 :password => "foobar",
+                 :password_confirmation => "foobar")
+    99.times do |n|
+      name = Faker::Name.name
+      email = "example-#{n+1}@railstutorial.org"
+      password = "foobar"
+      User.create!(:name => name, :email => email, 
+                  :password => password, :password_confirmation => password)
+    end                 
+  end
+end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -17,6 +17,10 @@ describe UsersController do
         @user = test_sign_in(Factory(:user))
         Factory(:user, :email => "another@email.com")
         Factory(:user, :email => "another@email.net")
+        
+        30.times do 
+          Factory(:user, :email => Factory.next(:email))
+        end
       end
       it "should be successful" do
         get :index
@@ -30,9 +34,19 @@ describe UsersController do
       
       it "should have element for each user" do
         get :index
-        User.all.each do |user|
+        User.paginate(:page => 1).each do |user|
           response.should have_selector("li", :content => user.name)
         end
+      end
+      
+      it "should paginate users" do
+        get :index
+        response.should have_selector("div.pagination")
+        response.should have_selector("span.disabled", :content => "Previous")
+        response.should have_selector("a", :href => "/users?page=2", 
+                                           :content => "2")
+        response.should have_selector("a", :href => "/users?page=2",
+                                           :content => "Next")              
       end
     end
   end
@@ -136,7 +150,7 @@ describe UsersController do
          flash[:success].should =~ /welcome to the sample app/i
       end
       
-      it "shpould sign user in" do
+      it "should sign user in" do
          post :create, :user => @attr
          controller.should be_signed_in
       end
