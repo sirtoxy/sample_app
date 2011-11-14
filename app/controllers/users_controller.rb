fa39212576a2958020e1aef5ae471b28d5ecec00
Anton Khotov <sirtoxy@gmail.com>
2011-11-14 14:53:31 +1100

    Destroy action added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
-  before_filter :authentificate, :only => [:index, :edit, :update]
+  before_filter :authentificate, :only => [:index, :edit, :update, :destroy]
   before_filter :correct_user, :only => [:edit, :update]
-
+  before_filter :admin_user, :only => [:destroy]
+  
   def index
     @users = User.paginate(:page => params[:page])
     @title = "All users"
@@ -29,18 +30,21 @@ class UsersController < ApplicationController
   end 
   
   def edit
-    @user = User.find(params[:id])
     @title = "Edit user"
   end
   
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(params[:user])
       redirect_to @user, :flash => { :success => "Profile updated"}
     else
       @title = "Edit user"
       render 'edit'
     end
+  end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    redirect_to users_path, :flash => { :success => "User deleted" }
   end
     
   private
@@ -52,5 +56,9 @@ class UsersController < ApplicationController
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_path) unless current_user?(@user)  
+    end
+    def admin_user
+      user = User.find(params[:id])
+      redirect_to(root_path) if (!current_user.admin? || current_user?(user))
     end      
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -154,7 +154,29 @@ describe User do
       
     end
   end 
+  
+  describe "admin attribute" do
+    before(:each) do
+      @user = User.create!(@attr)
+    end
+    
+    it "should respond to admin" do
+      @user.should respond_to(:admin)  
+    end
+    
+    it "should not be an admin" do
+       @user.should_not be_admin
+    end
+    
+    it "should be convertibale to admin" do
+      @user.toggle!(:admin)
+      @user.should be_admin
+    end
+    
+  end
+  
 end
+
 
 
 
@@ -169,5 +191,6 @@ end
 #  updated_at         :datetime
 #  encrypted_password :string(255)
 #  salt               :string(255)
+#  admin              :boolean         default(FALSE)
 #
 
