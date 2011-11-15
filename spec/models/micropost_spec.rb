5cea5ef2bd2a5156b3f5c0dde54f40649b3ac353
Anton Khotov <sirtoxy@gmail.com>
2011-11-15 16:57:18 +1100

    Microposts validation

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -24,6 +24,21 @@ describe Micropost do
     end
   end
   
+  describe "validations" do
+    it "should have the user_id" do
+      Micropost.new(@attr).should_not be_valid
+    end
+    
+    it "should require non blank content" do
+      @user.microposts.build(:content => "   ").should_not be_valid
+    end
+    
+    it "shoudl reject long content" do
+      @user.microposts.build(:content => "a" * 141).should_not be_valid
+      
+    end
+  end
+  
 end
 
 # == Schema Information
